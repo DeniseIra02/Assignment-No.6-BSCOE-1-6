2a1698cc964cc49428bba605610f577ff216ede7
DeniseIra02 <irajubilo@gmail.com>
2021-11-25 23:57:26 +0800

    Add For loop staement for the question.

--- Prog2.py
+++ Prog2.py
@@ -10,3 +10,12 @@ import random
 
 u_quest = {}
 k_score = 0
+
+for adding in range(10):
+    num_a = random.randint (0,99)
+    num_b = random.randint (0,99)
+    operator = '+'
+    _operator = operator
+    your_quest = str(num_a)+' '+str(_operator)+' '+str(num_b)
+    answer = eval(your_quest)
+    your_quest += ': '
