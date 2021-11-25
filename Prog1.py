267695ad248c0cb3e8533f471dbf69a0526e750c
DeniseIra02 <irajubilo@gmail.com>
2021-11-25 11:10:18 +0800

    Conditions of the Second number is the largest.

--- Prog1.py
+++ Prog1.py
@@ -3,7 +3,7 @@ def four_Num():
     num_b = float(input("Second Number: "))
     num_c = float(input("Third Number: "))
     num_d = float(input("Fourth Number: "))
-    
+
 # first number is the largest
     if num_a > num_b and num_a > num_c and num_a > num_d:
         if num_b > num_c and num_b > num_d:
@@ -23,4 +23,25 @@ def four_Num():
                 four_Num = num_a, num_d, num_c, num_b
             else:
                 four_Num = num_a, num_d, num_b, num_c
+            return four_Num
+
+# second number is the largest
+    if num_b > num_a and num_b > num_c and num_b > num_d:
+        if num_a > num_c and num_a > num_d:
+            if num_c > num_d:
+                four_Num = num_b, num_a, num_c, num_d
+            else:
+                four_Num = num_b, num_a, num_d, num_c
+            return four_Num
+        elif num_c > num_a and num_c > num_d:
+            if num_a > num_d:
+                four_Num = num_b, num_c, num_a, num_d
+            else: 
+                four_Num = num_b, num_c, num_d, num_a
+            return four_Num
+        else:
+            if num_c > num_a:
+                four_Num = num_b, num_d, num_c, num_a
+            else:
+                four_Num = num_b, num_d, num_a, num_c
             return four_Num
