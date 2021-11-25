20136d218b61e6c76d4b78c1f986e3a8b60a5c6c
DeniseIra02 <irajubilo@gmail.com>
2021-11-25 23:23:29 +0800

    Used print statements for the introduction in display.

---
+++ Prog2.py
@@ -0,0 +1,6 @@
+print("---------------------------------")
+print("Hi, Good day!")
+print("Welcome to ADDITION QUIZ!")
+print("---------------------------------")
+print("Please answer the questions below:")
+print("---------------------------------")
