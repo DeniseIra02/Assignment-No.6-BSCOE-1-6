4e44a9c9fa5e5180185f8664d247cc03661730d7
DeniseIra02 <irajubilo@gmail.com>
2021-11-25 10:47:01 +0800

    Assigning the four input numbers of the user into four variables using define function.

---
+++ Prog1.py
@@ -0,0 +1,5 @@
+def four_Num():
+    num_a = float(input("First Number: "))
+    num_b = float(input("Second Number: "))
+    num_c = float(input("Third Number: "))
+    num_d = float(input("Fourth Number: "))
